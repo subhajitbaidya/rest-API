962301a6b9a9116bee6e27363ae26cc826d768a6
Subhajit Baidya <subhajitbaidy43@gmail.com>
2024-11-28 18:33:55 +0530

    Model view controller completed for server side

--- controllers/user.js
+++ controllers/user.js
@@ -12,7 +12,46 @@ async function handleGetUserById(req, res) {
   return res.json(user);
 }
 
+async function handleUpdateUserById(req, res) {
+  await User.findByIdAndUpdate(req.params.id, { lastName: "Changed" });
+  res.status(200).json({ message: "User updated successfully" });
+}
+
+async function handleDeleteUserById(req, res) {
+  await User.findByIdAndDelete(req.params.id);
+  return res.status(200).json({ message: "User deleted successfully" });
+}
+
+async function handleCreateNewUser(req, res) {
+  const body = req.body;
+  if (
+    !body ||
+    !body.first_name ||
+    !body.last_name ||
+    !body.email ||
+    !body.gender ||
+    !body.job_title
+  ) {
+    return res.status(400).json({ msg: "All fields required" });
+  }
+
+  const result = await User.create({
+    firstName: body.first_name,
+    lastName: body.last_name,
+    email: body.email,
+    gender: body.gender,
+    jobTitle: body.job_title,
+  });
+
+  console.log("result", result);
+
+  return res.status(201).json({ msg: "success", id: result._id });
+}
+
 module.exports = {
   handleGetAllUsers,
   handleGetUserById,
+  handleUpdateUserById,
+  handleDeleteUserById,
+  handleCreateNewUser,
 };
--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,17 @@
 const express = require("express");
-const { handleGetAllUsers, handleGetUserById } = require("../controllers/user");
+const {
+  handleGetAllUsers,
+  handleGetUserById,
+  handleUpdateUserById,
+  handleDeleteUserById,
+  handleCreateNewUser,
+} = require("../controllers/user");
+
 const router = express.Router();
 
 //~routes
 
-//Server side rendering
+//?Server side rendering
 // router.get("/users", async (req, res) => {
 //     const allDBusers = await User.find({});
 //     const html = `
@@ -17,48 +24,15 @@ const router = express.Router();
 //     res.send(html);
 //   });
 
-//Rest API
+//!Rest API
 
 //*Client side rendering
-router.get("/", handleGetAllUsers);
+router.route("/").get(handleGetAllUsers).post(handleCreateNewUser);
 
 router
   .route("/:id")
   .get(handleGetUserById)
-  .patch(async (req, res) => {
-    await User.findByIdAndUpdate(req.params.id, { lastName: "Changed" });
-
-    res.status(200).json({ message: "User updated successfully" });
-  })
-  .delete(async (req, res) => {
-    await User.findByIdAndDelete(req.params.id);
-    return res.status(200).json({ message: "User deleted successfully" });
-  });
-
-router.post("/", async (req, res) => {
-  const body = req.body;
-  if (
-    !body ||
-    !body.first_name ||
-    !body.last_name ||
-    !body.email ||
-    !body.gender ||
-    !body.job_title
-  ) {
-    return res.status(400).json({ msg: "All fields required" });
-  }
-
-  const result = await User.create({
-    firstName: body.first_name,
-    lastName: body.last_name,
-    email: body.email,
-    gender: body.gender,
-    jobTitle: body.job_title,
-  });
-
-  console.log("result", result);
-
-  return res.status(201).json({ msg: "success" });
-});
+  .patch(handleUpdateUserById)
+  .delete(handleDeleteUserById);
 
 module.exports = router;
